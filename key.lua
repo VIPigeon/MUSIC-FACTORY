bb973a3a267fac24347a115ce20ba20c00db6902
VIPigeon <slv.bta@gmail.com>
2026-07-04 02:43:28 +0300

    добавил WASD и README

--- key.lua
+++ key.lua
@@ -1,20 +1,21 @@
 
+tic80_key = key
 key = {}
 
 function key.down()
-    return btn(1)
+    return btn(1) or tic80_key(KEY_S)
 end
 
 function key.up()
-    return btn(0)
+    return btn(0) or tic80_key(KEY_W)
 end
 
 function key.left()
-    return btn(2)
+    return btn(2) or tic80_key(KEY_A)
 end
 
 function key.right()
-    return btn(3)
+    return btn(3) or tic80_key(KEY_D)
 end
 
 function key.action()
