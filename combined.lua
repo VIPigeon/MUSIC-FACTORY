802f97adcb313ef725f5b5ada44c97d47dd431e7
VIPigeon <slv.bta@gmail.com>
2026-07-04 20:29:44 +0300

    теперь работает

--- combined.lua
+++ combined.lua
@@ -1224,8 +1224,334 @@ end
 
 Player.__index = Player
 -- END Player.lua
-require("Enemies") -- все противники в одном файле ⚛️
-require("Director") -- Дирижер 
+
+-- здесь смешным комментов нет. мне было не до шуток
+
+Enemy = {}
+function Enemy:move(x, y)
+    self.x = x
+    self.y = y
+end
+-- function Enemy:draw()
+--     spr(self.sprite[self.sprite.i].id, self.x, self.y, C0,1)
+-- end
+-- function Enemy:update()
+--     Anime.tick(self.sprite)
+-- end
+-- function Enemy:update()
+-- end
+-- function Enemy:draw()
+-- end
+-- function Enemy:attack()
+-- end
+
+function Enemy:earn_money()
+    game.money = game.money + 1
+    self.money_time = Enemy.MONEY_TIME -- для анимации
+end
+
+
+PistonEnemy = table.copy(Enemy)
+function PistonEnemy:new(x, y)
+    local object = {
+        x = x,
+        y = y,
+        sprite = table.copy(PistonEnemy.sprite.inactive),
+        hitbox = PistonEnemy.HITBOX,
+        interbox = PistonEnemy.INTERBOX,
+
+        is_active = false,
+        cost = Enemy.default_cost,
+
+        money_time = 0,
+        fear_time = 0, -- время страха призыва
+    }
+    setmetatable(object, self)
+    return object
+end
+
+function PistonEnemy:prepare()
+    if not self.is_active then
+        return
+    end
+
+    self.sprite = table.copy(PistonEnemy.sprite.prepare)
+end
+
+function PistonEnemy:attack()
+    -- if not self.is_active then
+    --     return
+    -- end
+
+    self.sprite = table.copy(PistonEnemy.sprite.attack)
+
+    local COUNT_BULLETS = 5
+    local SECTOR = 2*math.pi / COUNT_BULLETS
+    for i = 1, COUNT_BULLETS do
+        local bullet = Bullet:new(self.x+3, self.y+5)
+        local angle = math.random() * SECTOR + (i-1)*SECTOR
+        bullet.dx = math.cos(angle)
+        bullet.dy = math.sin(angle)
+        table.insert(game.bullets, bullet)
+    end
+end
+
+function PistonEnemy:release()
+    -- if not self.is_active then
+    --     return
+    -- end
+
+    self.sprite = table.copy(PistonEnemy.sprite.release)
+end
+
+function PistonEnemy:warning()
+    -- if not self.is_active then
+    --     return
+    -- end
+
+    self.sprite = table.copy(PistonEnemy.sprite.warning)
+end
+
+function PistonEnemy:draw()
+    if not self.is_active then
+        print(self.cost, self.x - 1, self.y + 10, Enemy.COST_COLOR, false,1, false)
+        -- Collision.draw_box(Collision.get_interbox_by_object(self))
+    end
+
+    spr(self.sprite[self.sprite.i].id, self.x, self.y, C0,1)
+    if self.money_time > 0 then
+        print("+1", self.x, self.y - 5, Enemy.MONEY_COLOR)
+    end
+end
+
+function PistonEnemy:update()
+    if not self.is_active then
+        self:activate_if_can()
+    else
+        Anime.tick(self.sprite)
+        self.money_time = Time.tick(self.money_time)
+        self.fear_time = Time.tick(self.fear_time)
+    end
+end
+
+function PistonEnemy:activate_if_can()
+    if self.cost == 0 and not self.is_active then
+        self.is_active = true
+        self.sprite = PistonEnemy.sprite.release
+        self.fear_time = Enemy.FEAR_TIME
+    end
+end
+
+PistonEnemy.__index = PistonEnemy
+
+
+CircleEnemy = table.copy(Enemy)
+function CircleEnemy:new(x, y)
+    local object = {
+        x = x,
+        y = y,
+        hitbox = CircleEnemy.HITBOX,
+        interbox = CircleEnemy.INTERBOX,
+
+        -- пуля не апдейтится и не отрисовывается,
+        -- так как не находится в списке пуль
+        chambered_bullet = Bullet:new(x-1, y-1),
+        -- -1 — костыль для пули 3×3
+
+        is_active = false,
+        cost = Enemy.default_cost,
+
+        -- вместо спрайта
+        status = 'release',
+        -- 'warning'
+        -- 'prepare'
+        -- 'attack'
+        -- 'release'
+        is_bullet_shot = false,
+
+        money_time = 0,
+        fear_time = 0,
+    }
+    setmetatable(object, self)
+    return object
+end
+
+function CircleEnemy:prepare()
+    -- if not self.is_active then
+    --     return
+    -- end
+    self.status = 'prepare'
+end
+
+function CircleEnemy:attack()
+    -- if not self.is_active then
+    --     return
+    -- end
+    self.status = 'attack'
+end
+
+function CircleEnemy:release()
+    -- if not self.is_active then
+    --     return
+    -- end
+    self.status = 'release'
+end
+
+function CircleEnemy:warning()
+    -- if not self.is_active then
+    --     return
+    -- end
+    self.status = 'warning'
+end
+
+function CircleEnemy:move(x, y)
+    self.x = x
+    self.y = y
+    self.chambered_bullet = Bullet:new(x-1, y-1)
+end
+
+
+function CircleEnemy:draw()
+    if not self.is_active then
+        -- Collision.draw_box(Collision.get_interbox_by_object(self))
+        TextWithOutline.print(self.cost, self.x - 5, self.y + 8, 2, Enemy.COST_COLOR,C0, 1, true)
+
+        Collision.draw_box(Collision.get_hitbox_by_object(self), 5)
+        self.chambered_bullet:draw(6)
+        return
+    end
+
+    Collision.draw_box(Collision.get_hitbox_by_object(self), 4)
+
+    if self.status == 'release' then
+        self.chambered_bullet:draw(1)
+    elseif self.status ~= 'attack' then
+        self.chambered_bullet:draw()
+    end
+
+    if self.money_time > 0 then
+        print("+1", self.x - 3, self.y - CircleEnemy.R - 7, Enemy.MONEY_COLOR)
+    end
+end
+
+function CircleEnemy:aimToPlayer()
+    local x = self.x
+    local y = self.y
+    local px, py = Collision.get_box_center(Collision.get_hitbox_by_object(game.player))
+
+    local dx = px - x
+    local dy = py - y
+
+    local length = math.sqrt(dx * dx + dy * dy)
+
+    if length == 0 then
+        return 0, 0
+    end
+
+    return dx / length, dy / length
+end
+
+function CircleEnemy:update()
+    if not self.is_active then
+        self:activate_if_can()
+    end
+
+    if self.status == 'prepare' then
+        local dx, dy = self:aimToPlayer()
+        self.chambered_bullet.x = self.x-1 - CircleEnemy.PULLBACK_DISTANCE * dx
+        self.chambered_bullet.y = self.y-1 - CircleEnemy.PULLBACK_DISTANCE * dy
+    elseif self.status == 'attack' and self.is_bullet_shot then
+        local dx, dy = self:aimToPlayer()
+
+        self.chambered_bullet.x = self.x-1-- + CircleEnemy.PULLBACK_DISTANCE * dx
+        self.chambered_bullet.y = self.y-1-- + CircleEnemy.PULLBACK_DISTANCE * dy
+
+
+        self.chambered_bullet.dx = dx
+        self.chambered_bullet.dy = dy
+        table.insert(game.bullets, self.chambered_bullet)
+        self.chambered_bullet = Bullet:new(self.x-1, self.y-1)
+        self.is_bullet_shot = false
+    elseif self.status == 'release' then
+        self.is_bullet_shot = true
+    --     self.chambered_bullet = Bullet:new(self.x-1, self.y-1)
+    end
+
+    self.money_time = Time.tick(self.money_time)
+    self.fear_time = Time.tick(self.fear_time)
+end
+
+function CircleEnemy:activate_if_can()
+    if self.cost == 0 and not self.is_active then
+        self.is_active = true
+        self.fear_time = Enemy.FEAR_TIME
+    end
+end
+
+CircleEnemy.__index = CircleEnemy
+
+
+
+Director = {}
+
+function Director.init()
+    -- будет работать по метроному
+    -- но сейчас все максимально просто, потом я это все снесу
+    -- upd: уже не актуально. Я уже все снес
+    Director.T = 60 / Settings.bpm
+    Director.t = Director.T
+
+    Director.beat_counter = 0
+end
+
+function Director:update()
+    Director.t = Time.tick(Director.t)
+    if Director.t == 0 then
+        Director.t = 60 / Settings.bpm
+
+        -- if Sheet.is_boss and Director.beat_counter >= 16*9 then
+        --     Sheet.load()
+        -- end
+
+        -- спавн бонусов
+        -- if #game.bonuses == 0 then
+        --     local x = math.random(5, 28) * 8
+        --     local y = math.random(4, 15) * 8
+        --     while 
+
+        --     table.insert(game.bonuses, Bonus:new())
+        -- end
+        --
+
+        -- мама, я хочу свою event-систему
+        -- нет, у нас есть event-система дома
+        -- event-система дома:
+        for part, e in pairs(game.enemies) do
+            if not e.is_active or e.fear_time > 0 then
+                goto continue
+            end
+            local current_i = (Director.beat_counter - 1) % #Sheet[part].normal + 1
+            local prev_i = (Director.beat_counter - 2) % #Sheet[part].normal + 1
+            local next_i = (Director.beat_counter) % #Sheet[part].normal + 1
+            local current_note = Sheet[part].normal[current_i]
+            local prev_note = Sheet[part].normal[prev_i]
+            local next_note = Sheet[part].normal[next_i]
+            if current_note then
+                e:attack()
+                current_note:play()
+                e:earn_money()
+            elseif prev_note then
+                e:release()
+            end
+            if next_note then
+                e:prepare()
+            end
+            ::continue::
+        end
+        Director.beat_counter = Director.beat_counter + 1
+    end
+end
+
 -- BEGIN key.lua
 
 tic80_key = key
